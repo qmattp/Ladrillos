a15f132a0962d3b229a82536d8c0b151f4f43260
qmattp <mateocamargoduran@gmail.com>
2023-11-18 22:53:50 -0500

    Actualización Nueva apk

--- brack.py
+++ brack.py
@@ -1,7 +1,11 @@
 #Librerias
 import pygame
 from pygame.locals import *
-
+import random 
+import time 
+#Codigo usado pyinstaller --onefile --noconsole brack.py
+#Para el ejecutable se instalo 
+#pyinstaller
 #Colores
 color=(144, 16, 144)
 #Pantalla
@@ -11,7 +15,7 @@ fondodepantalla= pygame.image.load('./pixel.jpg').convert()
 fuente = pygame.font.Font(None, 60)
 fuente2 = pygame.font.Font(None, 30)
 fuente3 = pygame.font.Font(None, 20)
-#Posiion
+#Posicion
 Posicionx=390
 Posiciony=350
 #Escritura
@@ -49,7 +53,7 @@ for y in range(Numero_nubes):
 sphere = pygame.image.load('./estrella.png')
 sphere = sphere.convert_alpha()
 sphere_rect = sphere.get_rect()
-sphere_start = (350, 250)
+sphere_start = (650, 250)
 sphere_speed = (3.0, 3.0)
 sx, sy = sphere_speed
 sphere_served = False
@@ -59,14 +63,22 @@ sphere_rect.topleft = sphere_start
 pad = pygame.image.load('./plataforma.png')
 pad = pad.convert_alpha()
 pad_rect = pad.get_rect()
-pad_rect[1] = screen.get_height() - 100
+pad_rect[1] = screen.get_height() - 150
 
 clock = pygame.time.Clock()
 game_over = False
 #///////////////
+if sphere_rect[1] >= screen.get_height() - sphere_rect.height:
+        sphere_served = False
+        sphere_rect.topleft = sphere_start
+#/////////
+tiempo = 0
+time= 0
+#Tiempo
 
 #INICIO DEL BUCLE PRINCIPAL
 while not game_over:
+    
     dt = clock.tick(80)
     screen.blit(fondodepantalla, [0,0])
     # Dibuja las nubes
@@ -83,8 +95,15 @@ while not game_over:
     if pressed[K_SPACE]:
         Posicionx=-40
         Posiciony=-40
+    #Posicion de vidas
+    Vida1=(10,630)
+    Vida2=(70,630)
+    Vida3=(130,630)
+    global Vidamenos
+    Vidamenos=0
+    #Logica de sprites
     
-    #Texto
+    #Texto en si
     text = "Pulsa espacio para jugar"
     mensaje = fuente.render(text, 1, (color))
     screen.blit(mensaje, (Posicionx, Posiciony))
@@ -94,13 +113,15 @@ while not game_over:
     Version = "V.1.O.Beta"
     Version2 = fuente3.render(Version, 1, (255,255,255))
     screen.blit(Version2, (1060, 630))
+    texto = "Fin del juego ganaste"
+    Fin_del_game_papa = fuente.render(texto, 1, (color))
     #Cerrar juego 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
-
 #FIN DEL BUCLE PRINCIPAL
 #LOGICA DEL JUEGO 
+    #Movimientos del jugador.
     pressed = pygame.key.get_pressed()
     if pressed[K_LEFT]:
         x -= 0.4 * dt
@@ -108,57 +129,63 @@ while not game_over:
         x += 0.4 * dt
     if pressed[K_SPACE]:
         sphere_served = True
-    # "colision" de paleta con la esfera
+    # "colision" de plataforma con la esfera
     if pad_rect[0] + pad_rect.width >= sphere_rect[0] >= pad_rect[0] and \
             sphere_rect[1] + sphere_rect.height >= pad_rect[1] and sy > 0:
         sy *= -1
-        # Incrementar dificultad
-        sy *= 1.1
-        sx *= 1.1
+        # Incrementar dificultad "Aumentar la velocidad de la pelota "
+        #sy *= 1.1
+        #sx *= 1.1
         continue
 
     delete_brick = None
-
+   
     for b in bricks:
         bx, by = b
-        # Destruir Ladrillos por colision de nuestra esfera
+        Primeravida=0
+        # Destruir Ladrillos por colision de la pelota 
         if bx <= sphere_rect[0] <= bx + brick_rect.width and \
                 by <= sphere_rect[1] <= by + brick_rect.height:
             delete_brick = b
 
             if sphere_rect[0] <= bx + 2:
                 sx *= -1
-                contador =+ 1
+                
             elif sphere_rect[0] >= bx + brick_rect.width - 2:
                 sx *= -1
+                
             if sphere_rect[1] <= by + 2:
                 sy *= -1
+                
             elif sphere_rect[1] >= bx + brick_rect.height - 2:
                 sy *= -1
+            
             break
 
+    if bx <= sphere_rect[0] <= bx + brick_rect.width and \
+                by <= sphere_rect[1] <= by + brick_rect.height:
+            delete_brick = b
+            
     if delete_brick is not None:
         bricks.remove(delete_brick)
-
-    # Top
+        
+    # Limite de arriba
     if sphere_rect[1] <= 0:
         sphere_rect[1] = 0
         sy *= -1
-
-    # Bottom
+        
+    # Limite de abajo
     if sphere_rect[1] >= screen.get_height() - sphere_rect.height:
-        # sphere_rect[1] = screen.get_height() - sphere_rect.height
-        # sy *= -1
-        # resetear juego
         sphere_served = False
         sphere_rect.topleft = sphere_start
-
-    # Left
+        Primeravida += 1
+    
+    # Limite derecho
     if sphere_rect[0] <= 0:
         sphere_rect[0] = 0
         sx *= -1
 
-    # Right
+    # Limite izquierdo
     if sphere_rect[0] >= screen.get_width() - sphere_rect.width:
         sphere_rect[0] = screen.get_width() - sphere_rect.width
         sx *= -1
@@ -168,6 +195,25 @@ while not game_over:
         # movi sphere
         sphere_rect[0] += sx
         sphere_rect[1] += sy
+    #Fin del juego
+    #if Primeravida == 1:
+       #Vida3=(-400,-400)
+    #if Primeravida== 2:
+        # Vida2=(-400,-400)
+    #if Primeravida == 3:
+        # Vida1=(-400,-400)
+    
+    #Vida = pygame.image.load('./Vidas.png')
+    #screen.blit(Vida, Vida1)
+    #screen.blit(Vida, Vida2)
+    #screen.blit(Vida, Vida3)
+   
+    #screen.blit(Fin_del_game_papa, (390, 350))
+    
+    #escribircontador=str(contadoralp)
+    #contador2 = fuente2.render(escribircontador, 1, (255,255,255))
+    #screen.blit(contador2, (200, 600))
+    #print(contador2)
 
     pygame.display.update()
 pygame.quit()
